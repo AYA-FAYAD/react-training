df273f60d3dbed44c85a269803349bb70fbe787c
aya fayad <ml.aya.fayad@gmail.com>
2024-04-01 01:58:20 +0300

    table2

--- comps/src/components/Table.tsx
+++ comps/src/components/Table.tsx
@@ -1,23 +1,25 @@
-function Table({ data }: any) {
-  const renderedRows = data.map((fruit: any) => {
-    return (
-      <tr className="border-b" key={fruit.name}>
-        <td className="p-3">{fruit.name}</td>
-        <td className="p-3">
-          <div className={`p-3 m-2 ${fruit.color}`}></div>
+function Table({ data, config, keyFn }: any) {
+  const renderedhader = config.map((column: any) => {
+    return <th key={column.label}>{column.label}</th>;
+  });
+  const renderedRows = data.map((rowData: any) => {
+    const renderedCells = config.map((column: any) => {
+      return (
+        <td className="p-2" key={column.label}>
+          {column.render(rowData)}
         </td>
-        <td className="p-3">{fruit.score}</td>
+      );
+    });
+    return (
+      <tr className="border-b" key={keyFn(rowData)}>
+        {renderedCells}
       </tr>
     );
   });
   return (
     <table className="table-auto border-spacing-2">
       <thead>
-        <tr className="border-b-2">
-          <th>Fruit</th>
-          <th>Color</th>
-          <th>Score</th>
-        </tr>
+        <tr className="border-b-2">{renderedhader}</tr>
       </thead>
       <tbody>{renderedRows}</tbody>
     </table>
--- comps/src/pages/TablePage.tsx
+++ comps/src/pages/TablePage.tsx
@@ -5,8 +5,28 @@ const fruits = [
   { name: "banna", color: "bg-yellow-500", score: 3 },
   { name: "Lime", color: "bg-green-500", score: 2 },
 ];
+
+const config = [
+  {
+    label: "Name",
+    render: (fruit: any) => fruit.name,
+  },
+
+  {
+    label: "Color",
+    render: (fruit: any) => <div className={`p-3 m-2 ${fruit.color}`} />,
+  },
+  {
+    label: "Score",
+    render: (fruit: any) => fruit.score,
+  },
+];
+
+const keyFn = (fruit: any) => {
+  return fruit.name;
+};
 function TablePage() {
-  return <Table data={fruits} />;
+  return <Table data={fruits} config={config} keyFn={keyFn} />;
 }
 
 export default TablePage;
